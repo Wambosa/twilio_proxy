56e5391c8a836f2d11cbe3ed4dc58c8a6d22582d
hdespiritu <hdespiritu@gmail.com>
2016-07-14 13:18:19 -0500

    Update calendar_service.js

--- src/calendar_service.js
+++ src/calendar_service.js
@@ -10,22 +10,37 @@ function sendCalendar(options){
     });
 }
 
-function generateCalendar(){
-    return Promise.resolve(
-`BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//Santander Consumer//Payment Reminder//EN
-X-WR-CALNAME:Loan Payment Due
-BEGIN:VEVENT
-UID:2016-07-13T15:50:52-05:00-%eE$!CA4N&CLtoo
-DTSTART:20160501T050000Z
-DTEND:20160502T050000Z
-SUMMARY:Loan Payment Due
-DESCRIPTION:Please make sure to make the mininmum payment by the due date.
-RRULE:FREQ=MONTHLY;COUNT=3
-END:VEVENT
-END:VCALENDAR
-`);
+function generateCalendar(clientData){
+	/* ex: {	
+	 	 title: "Loan Payment Due",
+		 summary: "Please make sure to make the mininmum payment by the due date",
+		 startDate:	"2016-05-01",
+		 loanLength: 3
+		}	
+	*/
+    var icsString = [
+        'BEGIN:VCALENDAR',
+        'VERSION:2.0',
+        'PRODID:-//Santander Consumer//Payment Reminder//EN'
+    ];
+
+    // todo: clamp the start to some reasonable time for the event to start.
+    var start = moment(clientData.startDate).startOf('day');
+    var dueDate = formatDate(start);
+    var endDate = formatDate(start.add(1, 'days'));
+
+    icsString.push('X-WR-CALNAME:' + clientData.title);
+    icsString.push('BEGIN:VEVENT');
+    icsString.push('UID:' + makeID());
+    icsString.push('DTSTART:' + dueDate);
+    icsString.push('DTEND:' + endDate);
+    icsString.push('SUMMARY:' + clientData.title);
+    icsString.push('DESCRIPTION:' + clientData.summary);
+    icsString.push('RRULE:FREQ=MONTHLY;COUNT=' + clientData.loanLength);
+    icsString.push('END:VEVENT');
+    icsString.push('END:VCALENDAR');
+
+    return Promise.resolve(icsString.join('\n'));
 }
 
 module.exports = {
